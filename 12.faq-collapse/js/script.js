aa22275c76955932949bc18e406c8bf89334df04
肖遥清 <1334823022@qq.com>
2022-07-06 15:24:49 +0800

    最新

--- 12.faq-collapse/js/script.js
+++ 12.faq-collapse/js/script.js
@@ -1,9 +1,9 @@
 
-
+//获取全部名为 faq-toggle的标签
 const toggles = document.querySelectorAll('.faq-toggle')
-
+//为每一个标签循环遍历添加一个点击事件，且动态生产元素（名为active）
 toggles.forEach(toggle => {
-    toggle.addEventListener('click', () =>{
+    toggle.addEventListener('click', () => {
         toggle.parentNode.classList.toggle('active')
     })
 })
